53bab0fd81f83526f9b92fbf02022ef49e0a9542
kuroya-saki <heiguzaoxi@gmail.com>
2020-10-21 05:23:05 +0000

    kadai-03

---
+++ kadai-03/index1.js
@@ -0,0 +1,16 @@
+
+
+// 生まれた日を入力
+const day = parseInt(process.argv[2])
+
+if (day % 5 === 0) {
+    console.log('あなたはマイペースな性格です')
+} else if (day % 5 === 1) {
+    console.log('あなたはせっかちな性格です')
+} else if (day % 5 === 2 ){
+    console.log('あなたはネガティブな性格です')
+} else if (day % 5 === 3){
+    console.log('あなたはアクティブな性格です')
+} else {
+    console.log('あなたはポジティブな性格です')
+}
---
+++ rensyu.js
@@ -0,0 +1,16 @@
+const score = 90
+if (score >= 90) {
+    console.log('S')
+} else if (score >= 80)
+    {console.log('A')
+      }
+        else if (score >= 70) {
+            console.lpg('B')
+        }else if (score >= 60){
+            console.log('C')
+        }
+        else {
+            console.log('F')
+        }
+　　　
+    
