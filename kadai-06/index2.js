19cc3253a396135894aa28da3bf8a079e61de184
kuroya-saki <heiguzaoxi@gmail.com>
2021-01-16 06:16:50 +0000

    課題6を追加

--- kadai-06/index2.js
+++ kadai-06/index2.js
@@ -1,28 +1,48 @@
+ try {
+    const str1 = "abc"  
+    JSON.parse(str1)
+ } catch (error) {
+     //パースできない：正しいJSONでないため。
+     //文字列だけのJSONは"で
+     console.log(error)
+ }
+
+ try {
+     const str2 = "'abc'"
+     JSON.parse(str2)
+ } catch (error){
+     //JSONの文字列は"で囲む必要があるため
+     console.log(error)
+ }
+
+ 
+     const str3 = '"abc"'
+     JSON.parse(str3)
+ 
+
+try{const str4 = '{"a": undefined}'
+     JSON.parse(str4)
+    }catch (error){
+       console.log(error)
+       //undefinedは値として使えないため。
+ }
+
+     const str5 = '{ "a": 123 }'
+     JSON.parse(str5)
+
 try {
-   const str1 = "abc"  
-   JSON.parse(str1)
+     const str6 = '{ a: 123"}'
+     JSON.parse(str6)
+    } catch (error) {
+       console.log(error)
+        // JSONのキーはダブルクォーテーションで囲む必要があるため。
+   }
+
+try {
+    const str7 ='{"func":() => console.log("HI!")}'
+JSON.parse(str7)
 } catch (error) {
-    //パースできない：正しいJSONでないため。
-    //文字列だけのJSONは"で
     console.log(error)
-}
-
-try{
-    const str2 = "'abc'"
-    JSON.paese(str2)
-} catch (error){
-    //JSONの文字列は"で囲む必要があるため
-    console.log(error)
-}
-
-const str3 = '"abc"'
-JSON.parse(str3)
-
-
-try{
-    const str5 = '{ "a": 123 }'
-    JSON.parse(str5)
-} catch (error) {
-    console
+    //ただしいJSONの文字列でないため。
 }
 
